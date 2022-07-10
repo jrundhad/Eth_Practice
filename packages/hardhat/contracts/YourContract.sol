b26f26ba5ded750216183fd6f5517c59adb052b9
Jeet Undhad <jrundhad@gmail.com>
2022-07-10 15:59:08 -0600

    learning about arrays

--- packages/hardhat/contracts/YourContract.sol
+++ packages/hardhat/contracts/YourContract.sol
@@ -18,7 +18,14 @@ contract YourContract {
 
   //how to declare a constant 
   uint public constant MY_UINT = 123;
+
+  // state variable num 
+  uint public num;
   
+  //need to send a transaction to change a state variable 
+  function set(uint _num) public {
+        num = _num;
+  }
 
   // set immutable variable in constructor
   constructor() payable {
@@ -41,7 +48,7 @@ contract YourContract {
 
   // Function to get the current count
   function get() public view  returns (uint){
-      return count;
+      return num;
   }
 
   // Function to increment count by 1
@@ -54,6 +61,31 @@ contract YourContract {
       // This function will fail if count = 0
       count -= 1;
   }
+
+  function foo(uint x) public pure returns (uint) {
+    if (x < 10) {
+        return 0;
+    } else if (x < 20) {
+        return 1;
+    } else {
+        return 2;
+    }
+  }
+
+  uint[] public arr = [1, 6, 3, 4, 5];
+  
+
+
+  function remove(uint _index) public returns (uint) {
+        require(_index < arr.length, "index out of bound");
+
+        for (uint i = _index; i < arr.length - 1; i++) {
+            arr[i] = arr[i + 1];
+        }
+        arr.pop();
+        uint x = arr[0];
+        return x;
+    }
 
   // to support receiving ETH by default
   receive() external payable {}
